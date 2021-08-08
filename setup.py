a57861533ffb549797ee288d7d83d5bb0ab17473
Edwin Vans <vans.edw@gmail.com>
2021-08-09 00:11:01 +1200

    Update setup.py
    This reverts commit 6636228ea5aa40c152180425048b552426dd7aca.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scplotlib", 
-    version="2.0.0",
+    version="2.0.1",
     author="Edwin Vans",
     author_email="vans.edw@gmail.com",
     description="A python package for visualizing the SingleCell data.",
