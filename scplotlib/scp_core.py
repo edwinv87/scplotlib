0a0e14afc8e1b1c2256b8c40f62bcb9915ef1c05
Edwin Vans <vans.edw@gmail.com>
2021-08-11 00:57:21 +1200

    Update scp_core.py

--- scplotlib/scp_core.py
+++ scplotlib/scp_core.py
@@ -1,6 +1,6 @@
 # Imports
 
-from .scp_themes import get_color
+from .scp_themes import get_marker, get_color
 
 """
 ========================
@@ -50,8 +50,9 @@ def ScatterPlot(    axis,
                     color_by, 
                     marker_style,
                     marker_size,
+                    marker_color,
                     legend_title
-                ):
+                    ):
 
 
     X = sc.getCellData(x)
@@ -62,33 +63,131 @@ def ScatterPlot(    axis,
     # To Do: Check if color_by is valid
 
     if (type(color_by) == str):
+
         cell_labels = sc.getNumericCellLabels(color_by)
+
         cell_types = sc.getDistinctCellTypes(color_by)
         
 
         if (type(cell_types[0]) != str):
-                cell_types = cell_types.astype(str)
+
+            cell_types = cell_types.astype(str)
+
 
         for i in range(1, len(cell_types) + 1):
+
             mask = (cell_labels == i)
+
             axis.scatter(   x = X[mask],
                             y = Y[mask],
                             c = get_color(i-1),
                             marker = marker_style,
                             s = marker_size,
-                            label = legend_title + cell_types[i-1] 
+                            label = legend_title + '-' + cell_types[i-1] 
                             )
 
     else:
         
         axis.scatter(   x = X,
                         y = Y,
-                        c = get_color(0),
+                        c = marker_color,
                         marker = marker_style,
                         s = marker_size,
-                        label = legend_title + 'cells' 
+                        label = legend_title
                         )
 
+        
+    
+
     # Temporarily remove marker size argument 26/07/2021
+
+    return axis
+
+
+
+
+def Scatter2D(  axis,
+                sc, 
+                x,
+                y,
+                color_by, 
+                marker_by, 
+                marker_style,
+                marker_size
+                ):
+
+    
+    if (type(marker_by) == str):
+
+        cell_labels = sc.getNumericCellLabels(marker_by)
+        cell_types = sc.getDistinctCellTypes(marker_by)
+
+        
+        if (type(cell_types[0]) != str):
+            cell_types = cell_types.astype(str)
+
+        # If marker_by == color_by?
+
+        if (marker_by == color_by):
+
+            for i in range(1, len(cell_types) + 1):
+                mask = (cell_labels == i)
+                axis = ScatterPlot( axis = axis,
+                                    sc = sc[mask.tolist()],
+                                    x = x,
+                                    y = y,
+                                    color_by = None,
+                                    marker_style = get_marker(i-1),
+                                    marker_size = marker_size,
+                                    marker_color = get_color(i-1),
+                                    legend_title = cell_types[i-1]
+                                    )
+
+            axis.legend(title = marker_by)
+        
+        else:
+
+            for i in range(1, len(cell_types) + 1):
+                mask = (cell_labels == i)
+                axis = ScatterPlot( axis = axis,
+                                    sc = sc[mask.tolist()],
+                                    x = x,
+                                    y = y,
+                                    color_by = color_by,
+                                    marker_style = get_marker(i-1),
+                                    marker_size = marker_size,
+                                    marker_color = get_color(0),
+                                    legend_title = cell_types[i-1]
+                                    )
+
+            if (type(color_by) == str):
+
+                axis.legend(title = marker_by + '-' + color_by)
+
+            else:
+                 
+                axis.legend(title = marker_by)
+
+    else:
+
+        axis = ScatterPlot(     axis = axis,
+                                sc = sc,
+                                x = x,
+                                y = y,
+                                color_by = color_by,
+                                marker_style = marker_style,
+                                marker_size = marker_size,
+                                marker_color = get_color(0),
+                                legend_title = 'cluster'
+                                )
+
+
+        if (type(color_by) == str):
+
+            axis.legend(title = color_by)
+
+
+    axis.set_ylabel(x)
+    axis.set_xlabel(y)
 
     return axis
